fe835334a5579f478cfb1b3e216724f43d43fc83
adam iqshan <omai.mohmed9@gmail.com>
2020-09-08 02:50:30 +0530

    finished design

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSelect(img) {
+    document.querySelector('.pepsi').src = img
+}
+
+function backColor(color) {
+    let section = document.querySelector('section');
+    section.style.background = color;
+}
